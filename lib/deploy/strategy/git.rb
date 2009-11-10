471b25a3492fd0f53635fe5d60e1e04cbfb8c490
Leigh Caplan <lcaplan@onehub.com>
2009-11-10 15:45:43 -0600

    Just some test code

---
+++ lib/deploy/strategy/git.rb
@@ -0,0 +1,13 @@
+module Capistrano
+  module Deploy
+    module Strategy
+
+      class Git < Remote
+        def deplizzle
+          puts 'Deploying!'
+        end
+      end
+      
+    end
+  end
+end
